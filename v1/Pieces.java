dd186f7837bb136afa2029101b976afe5284a023
sophiaei <seiden30@stuy.edu>
2022-06-01 12:11:53 -0400

    v1 prog

--- v1/Pieces.java
+++ v1/Pieces.java
@@ -1,1 +1,20 @@
+public class Piece{
+  private String[] _possibleMoves;
+  private String _location;
 
+  public Piece(){
+
+  }
+  public String getLocation(){
+    return _location;
+  }
+  public checkMoves(){
+
+  }
+  public move(String newLocation){
+
+  }
+  public isMove(){
+
+  }
+}
