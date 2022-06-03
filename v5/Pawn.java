c09415cb71da9060d723fb35a0181375e3ec9021
AmeerA0506 <aalnasser30@stuy.edu>
2022-06-03 12:35:45 -0400

    h

--- v5/Pawn.java
+++ v5/Pawn.java
@@ -1,9 +1,28 @@
 public class Pawn extends Piece{
   protected Location _current;
   public void checkMoves(){
-        if(_row==(4.5-2.5*color)){
-          _possibleMoves.add(new Location())
+    Location New=new Location(this._row+2*this._color,this._column);
+    Location New2=new Location(this._row+this._color,this._column);
+    Location New3=new Location(this._row+this._color,this._column-1);
+    Location New4=new Location(this._row+this._color,this._column+1);
+
+    if(!board.positionExists(New2)){ //up 1 move
+    _possibleMoves.add(New2);
+}
+
+        if(_row==(4.5-2.5*color)&&!board.positionExists(New2)){ // up 2 move
+          if(!board.positionExists(New)){
+          _possibleMoves.add(New);
         }
+      }
+      if(board.positionExists(New3){ //diagnol eat
+        _possibleMoves.add(New3);
+
+      }
+      if(board.positionExists(New4){ //diagnol eat pt 2
+        _possibleMoves.add(New4);
+
+      }
     }
 
   }
